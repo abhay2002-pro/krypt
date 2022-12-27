ab48b60a4454f3f2c4d0fee93bd83a49580b5a09
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-12-28 00:22:41 +0530

    smart contract deployed

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,8 +4,35 @@ pragma solidity ^0.8.0;
 
 // contract is similar to class
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     // event is similar to methods
     event Transfer(address from, address reciever, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiever, uint amount, string memory message, string memory keyword) public { //message is stored in memory of the transaction.
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiever, amount, message, block.timestamp, keyword)); // msg is reieved automatically when a function is called in blockchain
+
+        // to make transfer
+        emit Transfer(msg.sender, receiever, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory){
+        return transactions;
+    }
+
+    function getTransactionsCount() public view returns (uint256){
+        return transactionCount;
+    }
 }
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/AEHHW0zvyNg2E-38XtucrFwawd9eyc4z',
+      accounts: ['02bc32aee2d3352a9392b5cd24c91cdc0ba78375490d8ffdbcd3f408c8c97bab']
+    }
+  },
+}
+
